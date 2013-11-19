4390e1f991bc39387b188d92b9fcc91b417a625a
Andrew Udvare <audvare@gmail.com>
2013-11-18 17:42:02 -0800

    Rename file; bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='HttpExt',
-    version='0.1.0',
+    version='0.1.1',
     author='Andrew Udvare',
     author_email='audvare@gmail.com',
     py_modules=['httpext'],
     url='http://pypi.python.org/pypi/HttpExt/',
     license='LICENSE.txt',
     description='Helpers for downloading files.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
 )
